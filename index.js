3ee3491460921363f2fb469b77fca1c4e324f536
mcsordi <matheuscarissordi@gmail.com>
2023-02-27 13:59:58 -0300

    feat: after click button delete data of input

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const modalRegister = () => {
   document.getElementById("open-modal").style.display = "none";
   document.getElementById("closed-modal").style.display = "none";
 };
+const deleteInfos = () => {
+  setTimeout(() => {
+    [...(document.getElementById("nameInput").value = "")];
+    [...(document.getElementById("emailInput").value = "")];
+  }, 100);
+};
 
 const onclickButtonOpen = () => {
   const valueName = document.getElementById("nameInput").value;
@@ -11,7 +17,12 @@ const onclickButtonOpen = () => {
   const localSetEmail = localStorage.setItem("@Efront:email", valueEmail);
   const localGetName = localStorage.getItem("@Efront:user");
   const localGetEmail = localStorage.getItem("@Efront:email");
-  if (nameLength >= 3 && localGetEmail.includes("@")) {
+  deleteInfos();
+  if (
+    nameLength >= 3 &&
+    localGetEmail.includes("@") &&
+    localGetEmail.length > 5
+  ) {
     document.getElementById("open-modal").style.display = "flex";
   } else {
     document.getElementById("closed-modal").style.display = "flex";
@@ -28,4 +39,5 @@ const onclickModalClosed = () => {
 
 window.onload = () => {
   modalRegister();
+  deleteInfos();
 };
